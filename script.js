6f47a91a91a779a7ed96be0b6d057d0d6a8c654d
ShreyaMBelanekar42 <shreyaluvbelanekar@gmail.com>
2023-07-19 23:35:51 +0530

    added and styled navbar

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hello");
+function onOpen() {
+    var navbar = document.getElementById('navbar');
+    navbar.classList.add('show');
+}
+
+function onClose() {
+    var navbar = document.getElementById('navbar');
+    navbar.classList.remove('show');
+}
